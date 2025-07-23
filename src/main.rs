20968a64dbeb42c243b5b9bfc7c34037d38ab6c4
Eric Driussi <ericdriussi@gmail.com>
2025-07-23 18:29:06 +0100

    better game loop

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,63 @@
 use hangman::hangman::{GuessResult, Hangman};
 use hangman::state::GameState;
 use std::io;
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
-    let mut game = start_game();
+    clear_screen();
+    let (mut game, secret_word) = start_game();
 
     while game.state() == GameState::InProgress {
+        clear_screen();
+
         println!("Current state: {:?}", game.state());
         println!("Secret word: {}", game.display_word());
-        println!("Correctly guessed characters: {:?}", game.already_guessed());
         println!("Allowed failures left: {}", game.remaining_failures());
+        println!("Already guessed characters: {}", game.already_guessed());
 
-        println!("Enter your guess (single character):");
-        let guess = read_stdin().chars().next().unwrap_or_default();
+        println!();
+        println!("Take a guess!");
+        let guess = read_char();
 
+        clear_screen();
         match game.guess(guess) {
-            GuessResult::Correct => println!("Good guess!"),
-            GuessResult::Incorrect => println!("Wrong guess!"),
-            GuessResult::Duplicate => println!("You already guessed that character!"),
-            GuessResult::InvalidCharacter => println!("Invalid character! Please enter a letter."),
+            GuessResult::Correct => println!("Correct!"),
+            GuessResult::Incorrect => println!("Not quite..."),
+            GuessResult::Duplicate => println!("You already tried that!"),
+            GuessResult::InvalidCharacter => println!("WTF was that?! Just enter a character!"),
             GuessResult::GameNotInProgress => {
-                println!("The game is not in progress. Exiting...");
+                println!("This should never happen :(");
                 break;
             }
         }
+        sleep(Duration::from_secs(1));
+    }
+
+    if game.state() == GameState::Won {
+        println!("Yay! You guessed '{secret_word}'!");
+    } else {
+        println!("Game over! The secret word was: {secret_word}");
     }
 }
 
-fn start_game() -> Hangman {
+fn start_game() -> (Hangman, String) {
     println!("Enter the secret word:");
     let secret_word = read_stdin();
 
     println!("Enter the allowed number of failures:");
     let allowed_failures = read_int();
 
-    Hangman::init(&secret_word, allowed_failures).unwrap_or_else(|_| start_game())
+    match Hangman::init(&secret_word, allowed_failures) {
+        Ok(game) => (game, secret_word),
+        Err(e) => {
+            clear_screen();
+            println!("{e}");
+            println!("Try again...");
+            sleep(Duration::from_secs(1));
+            start_game()
+        }
+    }
 }
 
 fn read_stdin() -> String {
@@ -46,7 +69,19 @@ fn read_stdin() -> String {
 fn read_int() -> usize {
     let input = read_stdin();
     input.trim().parse::<usize>().unwrap_or_else(|_| {
-        eprintln!("That's doesn't make sense...");
+        println!("That's doesn't make sense... Try again!");
         read_int()
     })
+}
+
+fn read_char() -> char {
+    let input = read_stdin();
+    input.trim().parse::<char>().unwrap_or_else(|_| {
+        println!("Only ONE character! Try again!");
+        read_char()
+    })
+}
+
+fn clear_screen() {
+    print!("\x1B[2J\x1B[1;1H");
 }
